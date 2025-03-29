f1bac7ea4d00c4c93a4f21f7cba71866f6f820d5
mdy <h20096@e.hiroshima-cu.ac.jp>
2025-03-29 21:07:52 +0900

    tiptapエディタのフォルダ構成変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import "@/lib/tiptap/styles/tiptap.css";
+import "@/features/editor/styles/tiptap.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Center, VStack } from "@yamada-ui/react";
 import type { Metadata } from "next";
--- src/lib/tiptap/menus/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "../extensions/slash-menu";
-export * from "./bubble-menu";
